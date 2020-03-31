52c4d79926cb6d1ed3aa4641787a68e694cf9638
github username <github email address>
2020-03-31 17:28:17 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor :name, :roster
+    
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+
+   
+    def add_student (name, grade)
+        roster[grade] ||= []
+        roster[grade] << name
+    end
+
+    def grade (grade)
+        return roster[grade]
+    end
+
+    def sort
+        s_roster = {}
+        roster.each do |grade, student|
+            s_roster[grade] = student.sort
+        end
+        s_roster
+    end
+end
